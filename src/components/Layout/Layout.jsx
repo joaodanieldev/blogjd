11b413fb7ab8312a8b30ae812364f666110fe94b
Joao Daniel <joaodanieldev@gmail.com>
2021-08-04 11:30:49 -0300

    a instalaço dependencia

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../Header'
+
+export default ({ children, title }) => (
+  <div>
+    <Header title={title} />
+    <main role="main">{children}</main>
+  </div>
+);
+
